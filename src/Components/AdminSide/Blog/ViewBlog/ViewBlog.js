30db4814b7372ab56b43b9adf3bc1cdfd6a819b2
atul <atulpatare99@gmail.com>
2021-04-07 14:30:20 +0530

    changes in view and add

---
+++ src/Components/AdminSide/Blog/ViewBlog/ViewBlog.js
@@ -0,0 +1,124 @@
+import React, { useEffect, useState } from 'react';
+import "./styles.css";
+import GravityLogo from "../../../../Assets/GravityLogo.png"
+import { Avatar, Grid, Paper } from '@material-ui/core';
+import RealProfilePic from "../../../../Assets/ProfileReal.jpeg";
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import { useHistory, useParams } from 'react-router-dom';
+import API from '../../../../Utils/Utils';
+import { logout } from '../../../../Utils/SessionUtil';
+import axios from 'axios';
+
+
+const ViewBlog = () => {
+
+  const { id } = useParams();
+
+  const history = useHistory();
+
+  const [blog, setBlog] = useState([]);
+
+  useEffect(() => {
+    loadBlog();
+  }, []);
+
+  const loadBlog = async () => {
+    const token = sessionStorage.getItem("token");
+    if (token == null) {
+      history.push("/");
+      return;
+    }
+
+    // token exists 
+    const result = await axios.get(API.GET_BLOG, {
+      headers: {
+        'Authorization': 'Bearer ' + token
+      },
+      params: {
+        "id": id
+      }
+    });
+
+    if (result.data.data)
+      setBlog(result.data.data[0])
+  }
+
+  // ############################# OnClick Handlers ###############################
+
+  const handleMoveback = () => {
+    history.push('/blog')
+  }
+
+  const logoutHandler = () => {
+    logout(history)
+  }
+
+
+  return (
+
+    <div className="container cpr-cntnr">
+
+
+      {/* ################ ROW-1 ################## */}
+
+      <div className="row cpr-rw-1">
+
+        <div className="col-lg-10 col-md-6 col-sm-12 cpr-rw1-col-1">
+
+          <img className="img-fluid cpr-rw1-col-1-logo" src={GravityLogo} alt="" />
+
+        </div>
+
+        <div className="col-lg-2 col-md-6 col-sm-12 cpr-rw1-col-2">
+          <Avatar src={RealProfilePic} />
+          <p style={{ marginLeft: "1rem", fontSize: "12px" }}> {sessionStorage.getItem("user")} <br /><span><small style={{ cursor: "pointer" }} onClick={logoutHandler}>Logout</small></span></p>
+
+        </div>
+
+      </div>
+
+      {/* ################ ROW-2 ################## */}
+
+      <div className="row cpr-rw-2">
+
+        <div className="col-lg-10 col-md-6 col-sm-6 cpr-rw-2-col-1">
+          <h3 style={{ color: "gray" }}>View Blog</h3>
+        </div>
+
+
+        <div className="col-lg-2 col-md-6 col-sm-6 cpr-rw-2-col-2">
+
+          <div onClick={handleMoveback} className="bg-black-bck-arw cpr-back-arrow">
+            <ArrowBackIosIcon />
+          </div>
+          <p><small>Back</small></p>
+        </div>
+      </div>
+
+      {/* ################ ROW-3 ################## */}
+      <Paper className="paper-view-pack">
+        <div className="row">
+
+          <div className="col-lg-6 col-md-6 col-sm-12 view-pack-title">
+            <h1>BLOG DETAILS</h1>
+            {blog.photos && blog.photos.length > 0 &&
+              <img className="center" style={{ width: "200px", paddingTop: "10px", paddingBottom: "10px" }} src={API.GET_IMAGE + "?image=" + blog.photos[0]}></img>
+            }
+          </div>
+          <div className="col-lg-6 col-md-6 col-sm-12">
+            {
+              Object.entries(blog).map(([key, val]) =>
+                <Grid className="view-pack-key"><span className="vw-pack-key">{key}:</span>&nbsp;&nbsp;<span className="vw-pack-val">{val}</span></Grid>
+
+              )}
+          </div>
+        </div>
+
+
+      </Paper>
+
+    </div>
+  )
+}
+
+export default ViewBlog;
--- src/Components/AdminSide/Bookings/AddBooking/AddBooking.js
+++ src/Components/AdminSide/Bookings/AddBooking/AddBooking.js
@@ -6,6 +6,7 @@ import RealProfilePic from "../../../../Assets/ProfileReal.jpeg";
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import { useHistory } from 'react-router';
 import API from '../../../../Utils/Utils';
+import {logout} from '../../../../Utils/SessionUtil';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
@@ -196,7 +197,7 @@ const AddBooking = () => {
 
                 <div className="col-lg-2 col-md-6 col-sm-12 cpr-rw1-col-2">
                 <Avatar src={RealProfilePic}/>
-                <p style={{marginLeft: "1rem", fontSize: "12px"}}>Ram Singh <br/><span><small>Logout</small></span></p>
+                <p style={{ marginLeft: "1rem", fontSize: "12px" }}> {sessionStorage.getItem("user")} <br /><span><small style={{ cursor: "pointer"}} onClick={() => {logout(history)}}> Logout </small></span></p>
 
                 </div>
 
