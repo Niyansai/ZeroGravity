986e658155a5001895356cc4b3b9250718111b76
atul <atulpatare99@gmail.com>
2021-04-12 04:36:40 +0530

    corosel changes

--- src/Components/CustomerSide/PackageDetails/PkgCorousel/PkgCorousel.js
+++ src/Components/CustomerSide/PackageDetails/PkgCorousel/PkgCorousel.js
@@ -4,42 +4,67 @@ import img2 from "../../../../Assets/img2.png";
 import img3 from "../../../../Assets/img3.png";
 import img4 from "../../../../Assets/img4.png";
 
-const PkgCorousel = () => {
+import API from "../../../../Utils/Utils";
 
-    return (
+const PkgCorousel = ({ data }) => {
+  const images = [img2, img3, img4];
 
-        <div>
-           
-           <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img style={{height: "400px", width:"100%"}} src={img1} class="d-block pkg-corouse-img-size" alt="..."/>
-    </div>
-    <div class="carousel-item">
-      <img style={{height: "400px", width:"100%"}} src={img2} class="d-block pkg-corouse-img-size" alt="..."/>
-    </div>
-    <div class="carousel-item">
-      <img style={{height: "400px", width:"100%"}} src={img3} class="d-block pkg-corouse-img-size" alt="..."/>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-           
+  return (
+
+    <div>
+
+      <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+
+          < div class="carousel-item active">
+            <img style={{ height: "400px", width: "100%" }} src={img1} class="d-block pkg-corouse-img-size" alt="..." />
+          </div>
+
+          < div class="carousel-item">
+            <img style={{ height: "400px", width: "100%" }} src={img2} class="d-block pkg-corouse-img-size" alt="..." />
+          </div>
+
+          < div class="carousel-item">
+            <img style={{ height: "400px", width: "100%" }} src={img3} class="d-block pkg-corouse-img-size" alt="..." />
+          </div>
+
+          {data && data.length > 0 &&
+            < div class="carousel-item active">
+              <img style={{ height: "400px", width: "100%" }} src={API.GET_IMAGE + "?image=" + data[0].toString()} class="d-block pkg-corouse-img-size" alt="..." />
+            </div>
+
+          }
+
+          {
+            data && data.length > 0 &&
+            data.map((item, index) => {
+              return (
+                <div class="carousel-item">
+                  <img style={{ height: "400px", width: "100%" }} src={API.GET_IMAGE + "?image=" + item.toString()} class="d-block pkg-corouse-img-size" alt="..." />
+                </div>
+              )
+            })
+          }
 
         </div>
-    )
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+
+
+    </div >
+  )
 }
 
 export default PkgCorousel;
