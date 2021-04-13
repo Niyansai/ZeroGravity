2f3690c6ef0d7be5025839db88eea1ef1838f6ab
atul <atulpatare99@gmail.com>
2021-04-13 23:57:41 +0530

    changes in home page

--- src/Components/CustomerSide/Cards/SmallCard/SmallCard.js
+++ src/Components/CustomerSide/Cards/SmallCard/SmallCard.js
@@ -9,14 +9,17 @@ import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 import SkipNextIcon from '@material-ui/icons/SkipNext';
 import ProfileReal from '../../../../Assets/ProfileReal.jpeg';
+import { useHistory } from 'react-router';
 import "./styles.css";
+
+import API from '../../../../Utils/Utils';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
     marginTop: "2rem",
     borderBottom: '1px solid lightgray',
-    width: "350px", 
+    width: "350px",
     borderRadius: 0,
     boxShadow: '0px 2px 2px 6px, rgba(0, 0, 0, 0.4)'
   },
@@ -36,35 +39,40 @@ const useStyles = makeStyles((theme) => ({
     paddingLeft: theme.spacing(1),
     paddingBottom: theme.spacing(1),
   },
- 
+
 }));
 
-export default function SmallCard( {img} ) {
+export default function SmallCard({ id, name, img }) {
+  
   const classes = useStyles();
   const theme = useTheme();
 
+  const history = useHistory();
+
+  const moveToPackageDetails = () => {
+    history.push(`/packagedetails/${id}`);
+  }
+
   return (
     <Card className={classes.root}>
-        <CardMedia
+      <CardMedia
         className={classes.cover}
-        image={img}
+        image={API.GET_IMAGE + "?image=" + img[0]}
         title="Live from space album cover"
       />
       <div className={classes.details}>
         <CardContent className={classes.content}>
-          <Typography className="text-top-slidecard">
-            Start Date-End Date
-          </Typography>
+          {IDBCursor}
           <Typography className="text-bottom-slidecard">
-           Trip to the Himalayas
+            {name && name.toString()}
           </Typography>
 
         </CardContent>
         <div className={classes.controls}>
-          <button className="slider-card-btn">More</button>
+          <button className="slider-card-btn" onClick={(e) => { moveToPackageDetails() }}>More</button>
         </div>
       </div>
-      
+
     </Card>
   );
 }
